d0aee9d4a025c1c249f2e066b32092837d0c82af
iamleeon <165953114+iamleeon@users.noreply.github.com>
2024-04-26 13:59:16 +0300

    completeed the first task in module 4

---
+++ task_01.py
@@ -0,0 +1,25 @@
+# the function takes a file location as an argument and returns a tuple
+# with the total and average salaries
+def total_salary(path):
+    try:
+        with open(path, 'r', encoding="UTF-8") as salaries:  # open the file in the read-only mode
+            total_sal = 0
+            average_sal = 0
+            el_digit_count = 0
+            for line in salaries:  # cleans the text file from spaces, new line symbols and commas
+                line = line.strip()
+                line = line.split(',')
+                for el in line:  # checks for salaries and counts total
+                    if el.isdigit():
+                        total_sal += int(el)
+                        el_digit_count += 1
+                    else:
+                        continue
+                average_sal = round((total_sal / el_digit_count), 2)  # counts the average salary
+            return total_sal, average_sal
+    except FileNotFoundError:
+        print(f"FileNotFoundError. No such file or directory: {path}")
+
+
+if __name__ == '__main__':
+    total_salary()
