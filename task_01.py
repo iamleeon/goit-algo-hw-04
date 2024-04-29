03cbe5d70ad06fec741d768af1ef0495f0361d7e
iamleeon <165953114+iamleeon@users.noreply.github.com>
2024-04-29 15:15:09 +0300

    updated the solution according to the mentor's comments

--- task_01.py
+++ task_01.py
@@ -4,21 +4,19 @@ def total_salary(path):
     try:
         with open(path, 'r', encoding="UTF-8") as salaries:  # opens the file in the read-only mode
             total_sal = 0
-            average_sal = 0
             el_digit_count = 0
             for line in salaries:  # cleans the text file from spaces, new line symbols and commas
                 line = line.strip().split(',')
-                for el in line:  # checks for salaries and counts total
-                    if el.isdigit():
-                        total_sal += int(el)
-                        el_digit_count += 1
-                    else:
-                        continue
-                average_sal = round((total_sal / el_digit_count), 2)  # counts the average salary
+                total_sal += float(line[-1])
+                el_digit_count += 1
+            average_sal = round((total_sal / el_digit_count), 2)  # counts the average salary
             return total_sal, average_sal
-    except FileNotFoundError:
-        print(f"FileNotFoundError. No such file or directory: {path}")
+    except (FileNotFoundError, ZeroDivisionError) as error:
+        print(error)
 
 
-if __name__ == '__main__':
-    total_salary()
+try:
+    total, average = total_salary("salaries.txt")
+    print(f"Total salary: {total}. Average salary: {average}")
+except (ValueError, TypeError) as err:
+    print(err)
